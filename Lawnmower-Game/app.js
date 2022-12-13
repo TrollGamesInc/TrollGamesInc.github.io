f44469834bad019d42a81abfe82c6105d731f326
Troll Games Inc <118114028+TrollGamesInc@users.noreply.github.com>
2022-12-12 20:31:57 -0500

    Update app.js
    Added "Final Level, I promise" where you kill Terminators from the Fortnite level.

--- Lawnmower-Game/app.js
+++ Lawnmower-Game/app.js
@@ -33,7 +33,7 @@ var tileBasePrice = 150;
 var tickBasePrice = 5;
 
 var growthRateMultiplier = 1.2;
-var tickBaseMultiplier = 1.2;
+var tickBaseMultiplier = 1.5;
 var mowerRateMultiplier = 2.5;
 var mowerSizeMultiplier = 1.5;
 var tileSizeMultiplier = 3.5;
@@ -322,6 +322,7 @@ function addFields(){
     fields.push(new Area("Diamond", 0.85, 50000, [124, 124, 124], [124, 239, 228], "rgb(221, 206, 193)", 1000000000000, "Total Diamonds Mined: ", 2000, "Iron Pickaxe", "Ok - last one I swear."));
     fields.push(new Area("Gold", 0.95, 100000, [138, 202, 216], [211, 176, 0], "rgb(143, 158, 139)", 10000000000000, "Total Gold Panned: ", 5000, "Pan", "There's no rush ;)"));
     fields.push(new Area("Fortnite", 0.65, 5000, [255, 67, 50], [255, 211, 168], "rgb(100, 100, 100)", 100000000000000, "Total NPC's Killed: ", 10000, "Terminator", "I'll be back"));
+    fields.push(new Area("Final Stage, I promise", 0.65, 5000, [255, 67, 0], [255, 211, 0], "rgb(100, 100, 10)", 1000000000000000, "Total Terminators Killed: ", 100000, "Death", "STOP BEFORE I KILL YOU!"));
 }
 
 function setup(){
